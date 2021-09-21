6411b1365332b26c3679f87099d4f09dc24de22c
onlycjs <50040850+onlycjs@users.noreply.github.com>
2021-09-21 14:12:23 +0900

    html파일 업로드

--- work/common/js/common.js
+++ work/common/js/common.js
@@ -17,3 +17,16 @@ $(document).on('click','.calcul-btn > button',function() {
     $('.calcul-btn').removeClass('on');
     $(this).parent('.calcul-btn').addClass('on');
 });
+
+//사이드 메뉴 열기
+$('.open-depth-btn').click(function() {
+    $('.depth-con').addClass('opened-depth');
+});
+//사이드 메뉴 닫기
+$('.close-depth-btn').click(function() {
+    $('.depth-con').removeClass('opened-depth');
+});
+//드롭다운 메뉴
+$('.depth-item a').click(function() {
+    $(this).parent('.depth-item').children('.sub-depth').toggle();
+});
